6383a0d3546a22de95f113f9931ada75be5e260a
guckin <donald.stephen.mcguckin@gmail.com>
2021-12-25 00:25:30 -0500

    adds logging

--- src/lambda.ts
+++ src/lambda.ts
@@ -4,10 +4,12 @@ import {Handler} from 'aws-lambda';
 import {compose, log, tap} from './utility';
 
 export const getTopUrls: Handler<void, readonly string[]> = compose(
+    log,
     aggregateUrls(SeleniumDriver.forChrome),
     tap(log)
 );
 export const getAllRawText: Handler<string, readonly string[]> = compose(
+    log,
     getAllText(SeleniumDriver.forChrome),
     tap(log)
 );
--- src/utility.ts
+++ src/utility.ts
@@ -4,7 +4,15 @@ export type UnaryFn<I, O> = (value: I) => O;
 
 export type UnaryAsyncFn<I, O> = UnaryFn<I, Promise<O>>;
 
-export const compose = <A, B, C>(fn1: UnaryFn<A, B>, fn2: UnaryFn<B, C>): UnaryFn<A, C> => value => fn2(fn1(value));
+export function compose<A, B, C>(fn1: UnaryFn<A, B>, fn2: UnaryFn<B, C>): UnaryFn<A, C>;
+
+export function compose<A, B, C, D>(fn1: UnaryFn<A, B>, fn2: UnaryFn<B, C>, fn3: UnaryFn <C, D>): UnaryFn<A, D>;
+
+export function compose(...fns: ArraySize2Plus<UnaryFn<unknown, unknown>>): UnaryFn<unknown, unknown> {
+    return value => fns.reduce((prev: unknown, fn: UnaryFn<unknown, unknown>) => fn(prev), value);
+}
+
+export type ArraySize2Plus<T> = [T, T, ...Array<T>];
 
 export const tap = <T>(fn: (value: T) => void) => async (promise: Promise<T>): Promise<T> => {
     const value = await promise;
@@ -12,9 +20,10 @@ export const tap = <T>(fn: (value: T) => void) => async (promise: Promise<T>): P
     return value;
 };
 
-export const log = <T>(value: T): void => {
+export const log = <T>(value: T): T => {
     const inspected = inspect(value, { depth: null })
     console.log(inspected);
+    return value;
 };
 
 export const assertNever = (value: never) => {
